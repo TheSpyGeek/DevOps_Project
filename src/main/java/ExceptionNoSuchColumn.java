4155cfb7392899a8730f97b7053a509db14203b8
v4lt <victorbaverel@protonmail.com>
2018-04-10 14:33:56 +0200

    Test code coverage report.

--- src/main/java/ExceptionNoSuchColumn.java
+++ src/main/java/ExceptionNoSuchColumn.java
@@ -1,6 +1,6 @@
 public class ExceptionNoSuchColumn extends Exception {
     @Override
     public String getMessage() {
-        return "Impossible de réaliser une opération arithmétique sur une colonne de type String";
+        return "Impossible de réaliser une opération arithmétique sur une colonne de type String.";
     }
 }
