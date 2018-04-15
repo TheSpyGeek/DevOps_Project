65003c1ab1116e30157d9759c5aae0fb3338f3f3
TheSpyGeek <mathieurire@gmail.com>
2018-04-15 15:54:38 +0200

    changement de nom de l'application dans le maven

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,13 +2,15 @@ import java.util.ArrayList;
 
 class App {
 	public static void main(String args[]){
-		ArrayList<Comparable> i = new ArrayList<Comparable>();
-		String s = "3.5";
-		try {
-			Double.parseDouble(s);
-		}catch (Exception e){
-			e.printStackTrace();
-		}
+//		ArrayList<Comparable> i = new ArrayList<Comparable>();
+//		String s = "3.5";
+//		try {
+//			Double.parseDouble(s);
+//		}catch (Exception e){
+//			e.printStackTrace();
+//		}
+
+		System.out.println("Hello World");
 
 	}
 }
